4ebef6b70ad4d2d2f51891bcb1b28467d9aaca94
Ishita Tiwari <ishitatiwari060@gmail.com>
2026-02-19 00:40:54 +0530

    Add Home page

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Home() {
+  return (
+    <main className="min-h-screen p-4 flex flex-col items-center">
+      <h1 className="text-3xl font-bold mb-4">Welcome to FoodieExpress!</h1>
+      <p>Discover the best food around you and get it delivered fast.</p>
+    </main>
+  );
+}
+
+export default Home;
