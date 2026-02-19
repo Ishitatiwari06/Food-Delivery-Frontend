582bc888e3916d9bf7b9303e4cb596fbc057f429
Ishita Tiwari <ishitatiwari060@gmail.com>
2026-02-19 20:53:29 +0530

    updated home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,11 @@
 import React from "react";
-<<<<<<< HEAD
-
-=======
-import Card from "../components/Card.jsx";
->>>>>>> feature/frontend-ui
+import Card from "../components/Card";
 function Home() {
   return (
     <main className="min-h-screen p-4 flex flex-col items-center">
       <h1 className="text-3xl font-bold mb-4">Welcome to FoodieExpress!</h1>
       <p>Discover the best food around you and get it delivered fast.</p>
-<<<<<<< HEAD
-=======
       <Card />
->>>>>>> feature/frontend-ui
     </main>
   );
 }
