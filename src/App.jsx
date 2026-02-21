d94d6471c44ae07060a46a7a6547cf009a0d751a
Ishita Tiwari <ishitatiwari060@gmail.com>
2026-02-22 00:56:31 +0530

    feat(auth): implement login functionality

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Navbar from "./components/Navbar.jsx";
 import Footer from "./components/Footer.jsx";
 import Home from "./pages/Home.jsx";
 import Signup from "./pages/Signup.jsx";
-// import Login from "./pages/Login.jsx";
+import Login from "./pages/Login.jsx";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
@@ -15,7 +15,7 @@ function App() {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/signup" element={<Signup />} />
-            {/* <Route path="/login" element={<Login />} /> */}
+            <Route path="/login" element={<Login />} />
             {/* Add more routes here as you create more pages */}
           </Routes>
         </main>
--- src/services/authService.js
+++ src/services/authService.js
@@ -4,3 +4,8 @@ export const registerUser = async (data) => {
   const response = await axiosInstance.post("/createuser", data);
   return response.data;
 };
+
+export const loginUser = async (data) => {
+  const response = await axiosInstance.post("/login", data);
+  return response.data;
+};
