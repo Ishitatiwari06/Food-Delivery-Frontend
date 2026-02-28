4d25570b1b1d89559f8cfb5b896fae0d4a8136f5
Ishita Tiwari <ishitatiwari060@gmail.com>
2026-02-28 23:42:20 +0530

    changed path in home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,7 +8,7 @@ function Home() {
   const [search, setSearch] = useState("");
   const loadData = async () => {
     try {
-      const response = await fetch("http://localhost:5000/api/foodData",{
+      const response = await fetch(`${import.meta.env.VITE_API_URL}/foodData`,{
         method:"POST",
         headers: {
           "Content-Type": "application/json",
